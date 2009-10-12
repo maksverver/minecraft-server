86c1e783eb0ee5f6459eaf5a3eefc31afae352ee
Maks Verver <maksverver@geocities.com>
2009-10-12 17:11:11 +0200

    Moved some files around.

---
+++ common/blocks.h
@@ -0,0 +1,47 @@
+#ifndef BLOCKS_H_INCLUDED
+#define BLOCKS_H_INCLUDED
+
+#define BLOCK_EMPTY          0
+#define BLOCK_STONE_GREY     1
+#define BLOCK_GRASS          2
+#define BLOCK_DIRT           3
+#define BLOCK_ROCK           4
+#define BLOCK_WOOD           5
+#define BLOCK_SAPLING        6
+#define BLOCK_ADMINIUM       7
+#define BLOCK_WATER1         8
+#define BLOCK_WATER2         9
+#define BLOCK_LAVA1         10
+#define BLOCK_LAVA2         11
+#define BLOCK_STONE_YELLOW  12
+#define BLOCK_STONE_MIXED   13
+#define BLOCK_ORE1          14
+#define BLOCK_ORE2          15
+#define BLOCK_ORE3          16
+#define BLOCK_TRUNK         17
+#define BLOCK_LEAVES        18
+#define BLOCK_SPONGE        19
+#define BLOCK_GLASS         20
+#define BLOCK_COLORED1      21
+#define BLOCK_COLORED2      22
+#define BLOCK_COLORED3      23
+#define BLOCK_COLORED4      24
+#define BLOCK_COLORED5      25
+#define BLOCK_COLORED6      26
+#define BLOCK_COLORED7      27
+#define BLOCK_COLORED8      28
+#define BLOCK_COLORED9      29
+#define BLOCK_COLORED10     30
+#define BLOCK_COLORED11     31
+#define BLOCK_COLORED12     32
+#define BLOCK_COLORED13     33
+#define BLOCK_COLORED14     34
+#define BLOCK_COLORED15     35
+#define BLOCK_COLORED16     36
+#define BLOCK_FLOWER_YELLOW 37
+#define BLOCK_FLOWER_RED    38
+#define BLOCK_MUSHROOM      39
+#define BLOCK_TOADSTOOL     40
+#define BLOCK_GOLD          41
+
+#endif /* ndef BLOCKS_H_INCLUDED */
